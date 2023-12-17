7e05ed2c73d6d146da492d3d91761f43b36eaea2
Joe Walters <info@thenakeddeveloper.com>
2023-12-17 23:44:41 +0000

    Fixed root scan query

--- SimpleAPI.cs
+++ SimpleAPI.cs
@@ -1,6 +1,8 @@
 using FluentAssertions;
 using System.Net;
+using System.Net.Http.Headers;
 using WireMock;
+using WireMock.Matchers;
 using WireMock.RequestBuilders;
 using WireMock.ResponseBuilders;
 using WireMock.Server;
@@ -41,7 +43,7 @@ namespace Wiremock.Examples
                 .Given(Request.Create().WithPath("/tokens").UsingGet())
                 .RespondWith(Response.Create()
                         .WithStatusCode(HttpStatusCode.OK)
-                        .WithHeader("Content-Type", "application/json") //"text/plain")
+                        .WithHeader("Content-Type", "application/json")
                         .WithBody("{}")
                     );
         }
@@ -67,7 +69,7 @@ namespace Wiremock.Examples
                 .Given(Request.Create().WithPath("/tokens/*").UsingGet())
                 .RespondWith(Response.Create()
                         .WithStatusCode(HttpStatusCode.OK)
-                        .WithHeader("Content-Type", "application/json") //"text/plain")
+                        .WithHeader("Content-Type", "application/json")
                         .WithBody(BodyFactory, "application/json")
                     );
         }
@@ -87,6 +89,35 @@ namespace Wiremock.Examples
             }
         }
 
+        [Fact]
+        public async Task BodyTest()
+        {
+            // ARRANGE
+            HttpClient httpClient = new HttpClient();
+            BodyTestStub();
+
+            // ACT
+            string postValue = "{\"Id\":\"12345678\"}";
+            StringContent content = new StringContent(postValue, new MediaTypeHeaderValue("application/json"));
+            var result = await httpClient.PostAsync($"{_baseUri}/tokens", content);
+
+            // ASSERT
+            string body = await result.Content.ReadAsStringAsync();
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        private void BodyTestStub()
+        {
+            // https://github.com/WireMock-Net/WireMock.Net/wiki/Request-Matching-JsonPathMatcher
+            // https://docs.hevodata.com/sources/engg-analytics/streaming/rest-api/writing-jsonpath-expressions/#:~:text=A%20JSONPath%20expression%20begins%20with,%5D)%20called%20the%20bracket%20notation.
+            _mockServer
+                .Given(Request.Create().WithPath("/tokens").UsingPost().WithBody(new JsonPathMatcher("$.Id[?('123456678')]")))
+                .RespondWith(Response.Create()
+                        .WithStatusCode(HttpStatusCode.OK)
+                        .WithHeader("Content-Type", "application/json")
+                        .WithBody("true", "application/json")
+                    );
+        }
 
         ~SimpleAPI()
         {
