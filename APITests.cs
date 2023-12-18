f1dd065ffc7128756b8524cbe755d40e9c60805f
Joe Walters <info@thenakeddeveloper.com>
2023-12-18 00:13:17 +0000

    Better Setup and Teardown to free up port after every test

--- APITests.cs
+++ APITests.cs
@@ -1,5 +1,6 @@
 using FluentAssertions;
 using System.Net;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using WireMock;
 using WireMock.Matchers;
@@ -9,14 +10,18 @@ using WireMock.Server;
 
 namespace Wiremock.Examples
 {
-    public class SimpleAPI
+    public class APITests : IDisposable
     {
         private readonly WireMockServer _mockServer;
         private const int _mockPort = 9876;
         private const bool _mockSSL = false;
         private readonly string _baseUri;
 
-        public SimpleAPI()
+        /// <summary>
+        /// This is run before every test and must be torn down with the "Dispose" method
+        /// so that after every test the port is freed up for the next attempt
+        /// </summary>
+        public APITests()
         {
             string protocol = _mockSSL ? "https" : "http";
             _baseUri = $"{protocol}://localhost:{_mockPort}";
@@ -119,7 +124,44 @@ namespace Wiremock.Examples
                     );
         }
 
-        ~SimpleAPI()
+
+        [Fact]
+        public async Task TimeoutRootTest()
+        {
+            // ARRANGE
+            HttpClient httpClient = new HttpClient();
+            httpClient.Timeout = TimeSpan.FromSeconds(1); // Short timeout delay
+            TimeoutTestStub();
+
+            // ACT
+            string postValue = "{\"Id\":\"523456678\"}";
+            StringContent content = new StringContent(postValue, new MediaTypeHeaderValue("application/json"));
+
+            Func<Task> act = async () => await httpClient.PostAsync($"{_baseUri}/tokens", content);
+
+            // ASSERT
+            await act.Should()
+                .ThrowAsync<TaskCanceledException>();
+        }
+
+        private void TimeoutTestStub()
+        {
+            // Respond with a delay if a certain Id is given
+            _mockServer
+                .Given(Request.Create().WithPath("/tokens").UsingPost().WithBody(new JsonPathMatcher("$.Id[?('523456678')]")))
+                .RespondWith(Response.Create()
+                        .WithStatusCode(HttpStatusCode.OK)
+                        .WithHeader("Content-Type", "application/json")
+                        .WithBody("true", "application/json")
+                        .WithDelay(2000)
+                    );
+        }
+
+        /// <summary>
+        /// Must dispose of the mock after every test as it will block the 
+        /// next test from running as the port will be in use
+        /// </summary>
+        public void Dispose()
         {
             _mockServer.Stop();
             _mockServer.Dispose();
